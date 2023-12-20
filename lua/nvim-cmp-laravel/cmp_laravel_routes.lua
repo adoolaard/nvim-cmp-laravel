b20b5cdcf48bf71c94fceb6a570186ca8d55b483
Abel Doolaard <abel@adforal.nl>
2023-12-20 14:07:51 +0100

    rollback

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -15,61 +15,28 @@ function source.complete(self, request, callback)
   callback({ items = routes })  -- Geef de routes terug aan nvim-cmp
 end
 
-function get_framework_version(callback)
-  Job:new({
-    command = 'php',
-    args = {'artisan', '--version'},
-    on_exit = function(j, return_val)
-      local result = j:result()
-      local output = table.concat(result, " ")
-      if string.match(output, "Lumen") then
-        callback('lumen')
-      else
-        callback('laravel')
-      end
-    end,
-  }):start()
-end
-
-function extract_routes(content, framework)
+-- Haal Laravel routes op
+function source.get_laravel_routes()
   local routes = {}
-  local pattern
+  -- Verkrijg de huidige werkdirectory en voeg de pad naar het routes bestand toe
+  local routes_php_path = vim.loop.cwd() .. '/routes/web.php'
 
-  if framework == 'laravel' then
-    pattern = "->name%('([^']*)'%)"  -- Voor Laravel projecten
-  elseif framework == 'lumen' then
-    pattern = "%'as'%s*=>%s*%'([^']+)%'"  -- Voor Lumen projecten
-  end
+  local file = io.open(routes_php_path, "r")
+  if file then
+    local content = file:read("*all")
+    file:close()
 
-  for alias in string.gmatch(content, pattern) do
-    table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+    for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
+      -- Gebruik een statische waarde voor 'kind' of laat het weg
+      table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+    end
+  else
+    vim.notify("Kon het bestand niet openen: " .. routes_php_path)
   end
 
   return routes
 end
 
-function source.get_laravel_routes(callback)
-  -- Zorg ervoor dat de callback bestaat voordat we verder gaan
-  if not callback then
-    vim.notify("Geen callback functie doorgegeven aan get_laravel_routes")
-    return
-  end
-
-  get_framework_version(function(framework)
-    local routes_php_path = vim.loop.cwd() .. '/routes/web.php'
-    local file = io.open(routes_php_path, "r")
-    if not file then
-      vim.notify("Kon het bestand niet openen: " .. routes_php_path)
-      return
-    end
-
-    local content = file:read("*all")
-    file:close()
-
-    local routes = extract_routes(content, framework)
-    callback(routes)
-  end)
-end
 -- Deze functie wordt gebruikt door nvim-cmp om de source te identificeren
 function source.get_keyword_pattern()
   return [[\w+]]  -- Pas dit aan indien nodig voor je use-case
