c5c86c2ca489216eff2fbfafd46a608ff291f35b
Abel Doolaard <abel@adforal.nl>
2023-12-20 14:25:09 +0100

    better trigger

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -44,28 +44,6 @@ function source.is_laravel()
 	return is_laravel, is_lumen
 end
 
--- Haal Laravel routes op
--- function source.get_laravel_routes()
---   local routes = {}
---   -- Verkrijg de huidige werkdirectory en voeg de pad naar het routes bestand toe
---   local routes_php_path = vim.loop.cwd() .. '/routes/web.php'
---
---   local file = io.open(routes_php_path, "r")
---   if file then
---     local content = file:read("*all")
---     file:close()
---
---     for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
---       -- Gebruik een statische waarde voor 'kind' of laat het weg
---       table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
---     end
---   else
---     vim.notify("Kon het bestand niet openen: " .. routes_php_path)
---   end
---
---   return routes
--- end
-
 -- Haal Laravel routes op en gebruik is_laravel() om te checken of het een Laravel project is. Verander de regex om de juiste routes te krijgen.
 function source.get_laravel_routes()
 	local routes = {}
@@ -77,19 +55,6 @@ function source.get_laravel_routes()
 		local content = file:read("*all")
 		file:close()
 
-		-- if source.is_laravel() then
-		-- 	print("Laravel project")
-		-- 	for alias in string.gmatch(content, "->name%('([^']*)'%)") do
-		-- 		-- Gebruik een statische waarde voor 'kind' of laat het weg
-		-- 		table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
-		-- 	end
-		-- else
-		-- 	print("Lumen project")
-		-- 	for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
-		-- 		-- Gebruik een statische waarde voor 'kind' of laat het weg
-		-- 		table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
-		-- 	end
-		-- end
 		local is_laravel, is_lumen = source.is_laravel()
 		if is_laravel then
 			print("Dit is een Laravel project.")
@@ -99,7 +64,7 @@ function source.get_laravel_routes()
 			end
 
 			if is_lumen then
-				print("Meer specifiek, het is een Lumen project.")
+				print("Het is een Lumen project.")
                 for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
                     -- Gebruik een statische waarde voor 'kind' of laat het weg
                     table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
@@ -122,7 +87,7 @@ end
 
 -- Deze functie wordt gebruikt door nvim-cmp voor het sorteren van items
 function source.get_trigger_characters()
-	return { "." } -- Pas dit aan indien nodig voor je use-case
+	return { "route('" } -- Pas dit aan indien nodig voor je use-case
 end
 
 -- Deze functie wordt gebruikt om de source te identificeren (optioneel)
