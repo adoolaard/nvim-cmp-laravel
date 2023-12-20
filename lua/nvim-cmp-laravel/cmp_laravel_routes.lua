c69ce817577126e7ca7af59b4d8f55a30431d082
Abel Doolaard <abel@adforal.nl>
2023-12-20 14:11:23 +0100

    test framework

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -15,7 +15,48 @@ function source.complete(self, request, callback)
   callback({ items = routes })  -- Geef de routes terug aan nvim-cmp
 end
 
+-- Check de framework versie. Met "php artisan --version" staat er in een Lumen project ook "Lumen"
+function source.is_laravel()
+  local job = Job:new({
+    command = 'php',
+    args = { 'artisan', '--version' },
+    cwd = vim.loop.cwd(),
+    on_exit = function(j, return_val)
+      if return_val == 0 then
+        return true
+      else
+        return false
+      end
+    end
+  })
+
+  local result = job:sync()
+  return result[1] == 'Laravel Framework'
+end
+
 -- Haal Laravel routes op
+-- function source.get_laravel_routes()
+--   local routes = {}
+--   -- Verkrijg de huidige werkdirectory en voeg de pad naar het routes bestand toe
+--   local routes_php_path = vim.loop.cwd() .. '/routes/web.php'
+--
+--   local file = io.open(routes_php_path, "r")
+--   if file then
+--     local content = file:read("*all")
+--     file:close()
+--
+--     for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
+--       -- Gebruik een statische waarde voor 'kind' of laat het weg
+--       table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+--     end
+--   else
+--     vim.notify("Kon het bestand niet openen: " .. routes_php_path)
+--   end
+--
+--   return routes
+-- end
+
+-- Haal Laravel routes op en gebruik is_laravel() om te checken of het een Laravel project is. Verander de regex om de juiste routes te krijgen.
 function source.get_laravel_routes()
   local routes = {}
   -- Verkrijg de huidige werkdirectory en voeg de pad naar het routes bestand toe
@@ -26,9 +67,16 @@ function source.get_laravel_routes()
     local content = file:read("*all")
     file:close()
 
-    for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
-      -- Gebruik een statische waarde voor 'kind' of laat het weg
-      table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+    if source.is_laravel() then
+      for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
+        -- Gebruik een statische waarde voor 'kind' of laat het weg
+        table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+      end
+    else
+      for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
+        -- Gebruik een statische waarde voor 'kind' of laat het weg
+        table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+      end
     end
   else
     vim.notify("Kon het bestand niet openen: " .. routes_php_path)
