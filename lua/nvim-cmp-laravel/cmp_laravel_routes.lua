4350bbeac1cb1cfcb42e2e6ce4f02186300cbfdd
Abel Doolaard <abel@adforal.nl>
2023-12-20 15:38:39 +0100

    kp1000

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -83,9 +83,7 @@ end
 -- Deze functie wordt gebruikt door nvim-cmp om de source te identificeren
 function source.get_keyword_pattern()
 	-- return [[\w+]]
-    -- return [[\croute\w*]]
-    -- return [A-Za-z]+\('.*'\)
-    return [[\croute\w*]]+\('.*'\)
+    return [[\croute('\w*]]
 end
 
 -- Minimale lengte van de keyword om de source te triggeren
