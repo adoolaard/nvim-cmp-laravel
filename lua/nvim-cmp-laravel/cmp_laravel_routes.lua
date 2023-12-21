0fbada7e793f08bbd1d546e4ee422162d85848ac
Abel Doolaard <abel@adforal.nl>
2023-12-21 16:24:27 +0100

    test, with php artisan route:list

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -5,14 +5,9 @@ local source = {}
 
 -- Deze functie wordt aangeroepen door nvim-cmp om de source te initialiseren
 function source.new()
-	return setmetatable({}, { __index = source })
+    return setmetatable({}, { __index = source })
 end
 
--- Deze functie wordt aangeroepen door nvim-cmp om de beschikbare items te krijgen
--- function source.complete(self, request, callback)
--- 	local routes = source.get_laravel_routes() -- Roep je functie aan om de routes op te halen
--- 	callback({ items = routes }) -- Geef de routes terug aan nvim-cmp
--- end
 function source.complete(self, request, callback)
     local context = request.context
     local line = context.cursor_before_line
@@ -20,102 +15,97 @@ function source.complete(self, request, callback)
 
     -- Check of de lijn eindigt met " route('"
     if string.sub(line, 1, cursor_col - 1):match("route%('") then
-        local routes = source.get_laravel_routes() -- Haal routes op
-        callback({ items = routes }) -- Geef de routes terug aan nvim-cmp
+        source.get_laravel_routes(function(routes)
+            callback({ items = routes }) -- Geef de routes terug aan nvim-cmp
+        end)
     else
-        callback({ items = {'geen routes '} }) -- Geen matches, geef een lege lijst terug
+        callback({ items = {} }) -- Geen matches, geef een lege lijst terug
     end
 end
 
--- Check de framework versie. Met "php artisan --version" staat er in een Lumen project ook "Lumen"
-
-function source.is_laravel()
-	local is_laravel = false
-	local is_lumen = false
-
-	local job = Job:new({
-		command = "php",
-		args = { "artisan", "--version" },
-		cwd = vim.loop.cwd(),
-		on_exit = function(j, return_val)
-			local result = j:result()
-			if return_val == 0 then
-				local version_output = table.concat(result, " ")
-				if version_output:match("Laravel Framework") then
-					is_laravel = true
-					if version_output:match("Lumen") then
-						is_lumen = true
-					end
-				end
-			end
-		end,
-	})
-
-	job:sync() -- This will wait for the job to finish
-
-	-- Return two booleans: is_laravel, is_lumen
-	return is_laravel, is_lumen
-end
-
--- Haal Laravel routes op en gebruik is_laravel() om te checken of het een Laravel project is. Verander de regex om de juiste routes te krijgen.
-function source.get_laravel_routes()
-	local routes = {}
-	-- Verkrijg de huidige werkdirectory en voeg de pad naar het routes bestand toe
-	local routes_php_path = vim.loop.cwd() .. "/routes/web.php"
-
-	local file = io.open(routes_php_path, "r")
-	if file then
-		local content = file:read("*all")
-		file:close()
-
-		local is_laravel, is_lumen = source.is_laravel()
-		if is_laravel then
-			print("Dit is een Laravel project.")
-			for alias in string.gmatch(content, "->name%('([^']*)'%)") do
-				-- Gebruik een statische waarde voor 'kind' of laat het weg
-				table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
-			end
-
-			if is_lumen then
-				print("Het is een Lumen project.")
-                for alias in string.gmatch(content, "%'as'%s*=>%s*%'([^']+)%'") do
-                    -- Gebruik een statische waarde voor 'kind' of laat het weg
-                    table.insert(routes, { label = alias, kind = cmp.lsp.CompletionItemKind.Text })
+function source.is_laravel(callback)
+    local job = Job:new({
+        command = "php",
+        args = { "artisan", "--version" },
+        cwd = vim.loop.cwd(),
+        on_exit = function(j, return_val)
+            local result = j:result()
+            if return_val == 0 then
+                local version_output = table.concat(result, " ")
+                if version_output:match("Laravel Framework") then
+                    callback(true, version_output:match("Lumen") ~= nil)
+                else
+                    callback(false, false)
                 end
-			end
-		else
-			print("Dit is geen Laravel of Lumen project.")
-		end
-	else
-		vim.notify("Kon het bestand niet openen: " .. routes_php_path)
-	end
+            else
+                callback(false, false)
+            end
+        end,
+    })
 
-	return routes
+    job:start()
 end
 
--- Deze functie wordt gebruikt door nvim-cmp om de source te identificeren
+function source.get_laravel_routes(callback)
+    source.is_laravel(function(is_laravel, is_lumen)
+        if not is_laravel then
+            print("Dit is geen Laravel of Lumen project.")
+            callback({})
+            return
+        end
+
+        local artisan_command = "route:list --method=GET"
+        if is_lumen then
+            artisan_command = "route:list --columns=Verb --columns=NamedRoute --method=GET"
+        end
+
+        local job = Job:new({
+            command = "php",
+            args = { "artisan", artisan_command },
+            cwd = vim.loop.cwd(),
+            on_stdout = function(_, line)
+                if is_lumen then
+                    -- Lumen route parsing
+                    local verb, route = line:match("| (%w+)%s+| ([%w%.%-_]+)%s+|")
+                    if verb == "GET" and route then
+                        table.insert(routes, { label = route, kind = cmp.lsp.CompletionItemKind.Text })
+                    end
+                else
+                    -- Laravel route parsing
+                    local route = line:match("%s+GET|HEAD%s+([%w/%-_%.:]+)%s+")
+                    if route then
+                        table.insert(routes, { label = route, kind = cmp.lsp.CompletionItemKind.Text })
+                    end
+                end
+            end,
+            on_exit = function(j, return_val)
+                if return_val == 0 then
+                    callback(routes)
+                else
+                    vim.notify("Kan route lijst niet ophalen.")
+                    callback({})
+                end
+            end
+        })
+
+        job:start()
+    end)
+end
+
 function source.get_keyword_pattern()
-	-- return [[\w+]]
-    -- return [[\croute('\w*]]
     return [[\croute('\w+]]
 end
 
--- Minimale lengte van de keyword om de source te triggeren
 function source.get_keyword_length()
     return 3
 end
 
--- Deze functie wordt gebruikt door nvim-cmp voor het sorteren van items
--- function source.get_trigger_characters()
--- 	return { "." } -- Pas dit aan indien nodig voor je use-case
--- end
 function source.get_trigger_characters()
-    return { "'", "(", "." } -- Voeg enkele extra tekens toe die relevant zijn
+    return { "'", "(", "." }
 end
 
--- Deze functie wordt gebruikt om de source te identificeren (optioneel)
 function source.is_available()
-	return true
+    return true
 end
 
 return source
