191c6d25935d41739d3a3fcdf85fa2602905428f
Abel Doolaard <abel@adforal.nl>
2023-12-23 23:03:55 +0100

    again try both in 1

--- lua/nvim-cmp-laravel/cmp_laravel_views.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_views.lua
@@ -38,11 +38,93 @@ function source.get_laravel_views()
     return views
 end
 
+function source.is_laravel()
+	local is_laravel = false
+	local is_lumen = false
+
+	local job = Job:new({
+		command = "php",
+		args = { "artisan", "--version" },
+		cwd = vim.loop.cwd(),
+		on_exit = function(j, return_val)
+			local result = j:result()
+			if return_val == 0 then
+				local version_output = table.concat(result, " ")
+				if version_output:match("Laravel Framework") then
+					if version_output:match("Lumen") then
+						is_lumen = true
+					else
+						is_laravel = true
+					end
+				end
+			end
+		end,
+	})
+
+	job:sync() -- This will wait for the job to finish
+
+	return is_laravel, is_lumen
+end
+
+
+function source.get_laravel_routes()
+	local routes = {}
+	local is_laravel, is_lumen = source.is_laravel()
+
+	local command
+	if is_laravel then
+		print("Dit is een Laravel project.")
+		command = "php artisan route:list --method=GET --no-ansi --json"
+	elseif is_lumen then
+		print("Het is een Lumen project.")
+		command = "php artisan route:list --method=get --columns=NamedRoute --json"
+	else
+		print("Dit is geen Laravel of Lumen project.")
+		return routes
+	end
+
+	local handle = io.popen(command, "r")
+	if handle then
+		local result = handle:read("*all")
+		handle:close()
+
+		local success, parsed = pcall(vim.fn.json_decode, result)
+		if success then
+			for _, route in ipairs(parsed) do
+				local route_name
+				if is_laravel then
+					route_name = route.name
+				elseif is_lumen then
+					route_name = route.namedRoute
+				end
+
+				if route_name then
+					table.insert(routes, {
+						label = "route('" .. tostring(route_name) .. "')",
+						kind = cmp.lsp.CompletionItemKind.laravel_routes,
+					})
+				end
+			end
+		else
+			vim.notify("Kon de JSON output niet parsen.")
+		end
+	else
+		vim.notify("Kon het Artisan commando niet uitvoeren.")
+	end
+
+	return routes
+end
+
+
+
 -- Update the complete function
 function source:complete(params, callback)
     -- Check if the input matches "route('" or "return view('"
     local cursor_before_line = string.sub(params.context.cursor_before_line, 1, params.offset - 1)
-    if cursor_before_line:match("return view%('$") then
+    if cursor_before_line:match("route%('$") then
+        local routes = source.get_laravel_routes()
+        callback({ items = routes, isIncomplete = true })
+    elseif cursor_before_line:match("return view%('$") then
         local views = source.get_laravel_views()
         callback({ items = views, isIncomplete = true })
     else
@@ -55,9 +137,9 @@ function source.get_trigger_characters()
 end
 
 -- Deze functie wordt gebruikt om de source te identificeren (optioneel)
-function source.is_available()
-	return vim.bo.filetype == "php" and source.has_laravel_files()
-end
+-- function source.is_available()
+-- 	return vim.bo.filetype == "php" and source.has_laravel_files()
+-- end
 
 -- Controleer of bepaalde Laravel-bestanden aanwezig zijn in de huidige werkdirectory
 function source.has_laravel_files()
--- lua/nvim-cmp-laravel/init.lua
+++ lua/nvim-cmp-laravel/init.lua
@@ -1,17 +1,17 @@
 local cmp = require("cmp")
-local cmp_laravel_routes = require("nvim-cmp-laravel.cmp_laravel_routes")
+-- local cmp_laravel_routes = require("nvim-cmp-laravel.cmp_laravel_routes")
 local cmp_laravel_views = require("nvim-cmp-laravel.cmp_laravel_views")
 print("Setting up Laravel autocomplete...")
 
 local function setup()
-	cmp.register_source("laravel_routes", cmp_laravel_routes.new())
+	-- cmp.register_source("laravel_routes", cmp_laravel_routes.new())
     cmp.register_source("laravel_views", cmp_laravel_views.new())
 
 	local sources = cmp.get_config().sources or {}
 	table.insert(
 		sources,
 		{
-			name = "laravel_routes",
+			-- name = "laravel_routes",
             name = "laravel_views",
 		}
 	)
