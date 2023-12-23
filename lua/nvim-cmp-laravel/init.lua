8fff9913f599deb8e287b7a021a2d593f7cc819f
Abel Doolaard <abel@adforal.nl>
2023-12-23 22:28:28 +0100

    test fix

--- lua/nvim-cmp-laravel/init.lua
+++ lua/nvim-cmp-laravel/init.lua
@@ -1,11 +1,11 @@
 local cmp = require("cmp")
 local cmp_laravel_routes = require("nvim-cmp-laravel.cmp_laravel_routes")
-local cmp_laravel_views = require("nvim-cmp-laravel.cmp_laravel_views")
+-- local cmp_laravel_views = require("nvim-cmp-laravel.cmp_laravel_views")
 print("Setting up Laravel autocomplete...")
 
 local function setup()
 	cmp.register_source("laravel_routes", cmp_laravel_routes.new())
-    cmp.register_source("laravel_views", cmp_laravel_views.new())
+    -- cmp.register_source("laravel_views", cmp_laravel_views.new())
 
 	local sources = cmp.get_config().sources or {}
 	table.insert(
