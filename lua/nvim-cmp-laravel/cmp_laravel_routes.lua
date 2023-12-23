15c9eab29d5bb88027006623d9e49709f750ae19
Abel Doolaard <abel@adforal.nl>
2023-12-23 19:59:53 +0100

    test route('

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -112,7 +112,7 @@ end
 
 -- Deze functie wordt gebruikt door nvim-cmp om de source te identificeren
 function source.get_keyword_pattern()
-	return [[\%(\croute('\)\@<=\k*]]
+	return [[\%(\croute(\)\@<=\k*]]
 end
 
 function source.get_keyword_length()
