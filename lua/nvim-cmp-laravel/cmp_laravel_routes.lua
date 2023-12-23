60330643a6ec6c4457ddea655fa2500b62a9a5fa
Abel Doolaard <abel@adforal.nl>
2023-12-23 20:20:42 +0100

    test

--- lua/nvim-cmp-laravel/cmp_laravel_routes.lua
+++ lua/nvim-cmp-laravel/cmp_laravel_routes.lua
@@ -114,7 +114,7 @@ end
 -- Deze functie wordt gebruikt door nvim-cmp om de source te identificeren
 function source.get_keyword_pattern()
 	-- return [[\%(\croute('\)\@<=\k*]]
-	return [[\%(\croute('\)\@<=*]]
+	return [[\%(\croute('\)\@<=\k*]]
 end
 
 function source.get_keyword_length()
@@ -127,12 +127,17 @@ end
 -- 	return { "'", "(", "." }
 -- end
 function source.get_trigger_characters()
-    -- check of de lijn begint met "route("
-    if vim.fn.matchstr(vim.fn.getline('.'), [[\%(\croute('\)\@<=\k*]]) == 0 then
+    local context = vim.api.nvim_get_current_line()
+    local line = context.cursor_before_line
+
+    -- Kijk of "route('" in de huidige regel voorkomt en of de cursor zich voorbij het einde van "route('" bevindt
+    local _, route_prefix_end = line:find("route%('")
+    if route_prefix_end and context.cursor.col > route_prefix_end then
         return { "'", "(", "." }
     else
         return {}
     end
+
 end
 
 
